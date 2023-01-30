1dfcc9902e1044ccb4c23df98803d9c8ec9a6fbb
n2jato00 <n2jato00@students.oamk.fi>
2023-01-30 14:58:01 +0200

    add calculate function

--- src/App.js
+++ src/App.js
@@ -2,20 +2,49 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [weight, setWeight] = useState(0)
+  const [weight, setWeight] = useState(90)
   const [gender, setGender] = useState('male')
-  const [bottles, setBottles] = useState(0)
+  const [bottles, setBottles] = useState(1)
   const [time, setTime] = useState(0)
   const [result, setResult] = useState(0)
 
 
+  function calculate(e) {
+    e.preventDefault();
+    let alcohol = 0;
+    let litre = 0
+    let grams = 0
+    let burning = 0
+    let gramsresult = 0
 
+    if (gender === 'male') {
+      litre = bottles * 0.33
+      grams = litre * 8 * 4.5
+      burning = weight / 10
+      gramsresult = grams - (burning * time)
 
+      alcohol = gramsresult / (weight * 0.7)
+    }
+    else {
+      litre = bottles * 0.33
+      grams = litre * 8 * 4.5
+      burning = weight / 10
+      gramsresult = grams - (burning * time)
+
+      alcohol = gramsresult / (weight * 0.6)
+    }
+    if (alcohol < 0) {
+      alcohol = 0
+
+    }
+    setResult(alcohol);
+  }
+  
 
   return (
     <>
       <h3>Veren alkoholipitoisuus</h3>
-      <form>
+      <form onSubmit={calculate}>
         <div>
           <label>Paino (kg): </label>
           <input name='weight' type='number' step='1' value={weight} onChange={e => setWeight(e.target.value)} />
@@ -38,6 +67,7 @@ function App() {
         <div>
           <label>Aika aloittamisesta (h) </label>
           <select name='time' value={time} onChange={e => setTime(e.target.value)}>
+            <option value='0'>0</option>
             <option value='1'>1</option>
             <option value='2'>2</option>
             <option value='3'>3</option>
